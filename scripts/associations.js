3159c4c41b53aef9848ae5e9ea51a01ecb0203d3
Zenitude <mollet.rudy@gmail.com>
2022-10-04 16:47:47 +0200

    First commit

---
+++ scripts/associations.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll('.buttonTabs button');
+const sections = document.querySelectorAll('.contentTabs section');
+
+console.log(sections);
+
+for (let i = 0 ; i < buttons.length ; i++)
+{
+    buttons[i].addEventListener('click', () => {
+        
+        if(!sections[i].classList.contains('activeTabs'))
+        {
+            sections.forEach(section => {
+                
+                section.classList.remove('activeTabs');
+    
+            });
+
+            sections[i].classList.add('activeTabs');
+        }
+
+    });
+}
