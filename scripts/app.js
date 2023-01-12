1db368a285d4dfe6f144324acbede84ba8c46a54
Zenitude <mollet.rudy@gmail.com>
2023-01-12 09:23:52 +0100

    update

--- scripts/app.js
+++ scripts/app.js
@@ -45,62 +45,63 @@ if(document.querySelector('.containerClips'))
     const clipsContainers = document.querySelectorAll('.clips');
     const page = document.querySelector('.pagination button');
 
+    class Clip {
+        constructor(linkClip, srcVideo, views, dateHour, srcImageStreamer, titleVideo, nameStreamer, activityStreamer, containerClip)
+        {
+            this.linkClip = linkClip;
+            this.srcVideo = srcVideo;
+            this.views = views;
+            this.dateHour = dateHour;
+            this.srcImageStreamer = srcImageStreamer;
+            this.titleVideo = titleVideo;
+            this.nameStreamer = nameStreamer;
+            this.activityStreamer = activityStreamer;
+            this.containerClip = containerClip;
+        }
+
+        createClips() {
+            const views = new Intl.NumberFormat().format(this.views);
+            const date = new Date(this.dateHour).toLocaleDateString();
+            const hours = new Date(this.dateHour).toLocaleTimeString();
+
+            const card = document.createElement('a');
+            card.setAttribute('href', this.linkClip);
+            card.setAttribute('class', 'card');
+            
+            card.innerHTML = `           
+                <video src="${this.srcVideo}" controls></video>
+                
+                <div class="desc">
+                    <div><span>${views} Vues</span><span>${date} à ${hours}</span></div>
+                    <div class="title">
+                        <img src="${this.srcImageStreamer}" alt="Image logo de ${this.nameStreamer}">
+                        <h2>${this.titleVideo}</h2>
+                    </div>
+                    <p><span>${this.nameStreamer}</span> - <span>${this.activityStreamer}</span></p>
+                </div>
+            `;
+
+            this.containerClip.appendChild(card);
+        }
+        
+    }
+
     clipsContainers.forEach(container => {
         
         for(let i = 0 ; i < 23 ; i++)
         {
             // Card
-            const card = document.createElement('a');
-            card.setAttribute('href', 'https://www.youtube.com');
-            card.setAttribute('class', 'card');
-            container.appendChild(card);
-        
-            // Container Video
-            const containerVideo = document.createElement('div');
-            containerVideo.setAttribute('class', 'video');
-            card.appendChild(containerVideo);
-        
-            // Video
-            const video = document.createElement('video');
-            video.setAttribute('controls', '');
-            containerVideo.appendChild(video);
-        
-            // Container Description
-            const containerDescription = document.createElement('div');
-            containerDescription.setAttribute('class', 'desc');
-            card.appendChild(containerDescription);
-        
-            // Views and Datetime
-            const viewsAndDatetime = document.createElement('div');
-            containerDescription.appendChild(viewsAndDatetime);
-        
-            const spanViews = document.createElement('span');
-            spanViews.innerHTML = '1 000 000 Vues';
-            viewsAndDatetime.appendChild(spanViews);
-        
-            const spanDatetime = document.createElement('span');
-            spanDatetime.innerHTML = '01/01/2022 à 01:00:00';
-            viewsAndDatetime.appendChild(spanDatetime);
-        
-            // Image Streamer and Title
-            const streamerAndTitle = document.createElement('div');
-            streamerAndTitle.setAttribute('class', 'title');
-            containerDescription.appendChild(streamerAndTitle);
-        
-            const streamer = document.createElement('img');
-            streamer.setAttribute('src', 'https://static-cdn.jtvnw.net/jtv_user_pictures/2895647a-2e57-45ac-a42c-0a837d42f9a1-profile_image-300x300.png');
-            streamer.setAttribute('alt', ''),
-            streamerAndTitle.appendChild(streamer);
-        
-            const title = document.createElement('h2');
-            title.innerHTML = 'Titre video';
-            streamerAndTitle.appendChild(title);
-        
-            // Name Streamer and Activities
-            const streamerAndActivities = document.createElement('p');
-            streamerAndActivities.innerHTML = 'Nom Streamer - Activité';
-            containerDescription.appendChild(streamerAndActivities);
-        
+            let card = new Clip(
+                'https://www.youtube.com', 
+                '', 
+                1000000, 
+                '01/01/2022 01:00:00', 
+                'https://static-cdn.jtvnw.net/jtv_user_pictures/2895647a-2e57-45ac-a42c-0a837d42f9a1-profile_image-300x300.png', 
+                'Titre video', 
+                'Nom streamer', 
+                'Activité',
+                container
+            ).createClips();
         }
     });
 
@@ -165,7 +166,6 @@ if(document.querySelector('.containerShop'))
     
 
     radios.forEach(radio => {
-        console.log(radio.children)
         radio.children[1].addEventListener('focusin', () => {
             radio.children[0].classList.add('selected')
         })
@@ -176,4 +176,14 @@ if(document.querySelector('.containerShop'))
         })
         
     })
+
+    /* Quantity */
+    const quantityTab = document.querySelectorAll('.inputQuantity'); // Tableau contenant tous les input
+    const moreTab = document.querySelectorAll('.moreQuantity'); // Tableau contenant tous les boutons +
+    const lessTab = document.querySelectorAll('.lessQuantity'); // Tableau contenant tous les boutons -
+
+    for(let i = 0 ; i < quantityTab.length ; i++) {
+        moreTab[i].addEventListener('click', () => quantityTab[i].value >= 20 ? quantityTab[i].value = 20 : quantityTab[i].value++);
+        lessTab[i].addEventListener('click', () => quantityTab[i].value <= 1 ? quantityTab[i].value = 1 : quantityTab[i].value--);
+    }
 }
